279c3f18915845390393f4e9303896e33a20c0ca
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-06 17:20:56 +0200

    Misie

--- src/main/java/com/sda/arrpl2/Bear.java
+++ src/main/java/com/sda/arrpl2/Bear.java
@@ -2,8 +2,22 @@ package com.sda.arrpl2;
 
 public class Bear {
     public static void main(String[] args) {
-        String imię;
-        int wiek;
-    }
+        Bear Marian = new Bear();
+        String imię = "Marian";
+        int wiek = 32;
 
+
+        Bear Olek = new Bear();
+        String imię_2 = "Olek";
+        int wiek_2 = 2;
+
+        Bear Filipek = new Bear();
+        String imię_3 = "Filipek";
+        int wiek_3 = 10;
+
+        Bear Teddy = new Bear();
+        String imię_4 = "Teddy";
+        int wiek_4 = 3;
+
+    }
 }
