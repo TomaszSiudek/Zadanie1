66c6b3761fa937a16ea371d89407f85e0f1c2567
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-06 18:09:45 +0200

    Misie

--- src/main/java/com/sda/arrpl2/Bear.java
+++ src/main/java/com/sda/arrpl2/Bear.java
@@ -1,23 +1,11 @@
 package com.sda.arrpl2;
 
+import java.util.Scanner;
+
+
 public class Bear {
-    public static void main(String[] args) {
-        Bear bear1 = new Bear();
-        bear1.imie = "Marian";
-        bear1.wiek = 32;
 
-
-        Bear bear2 = new Bear();
-        bear2.imię = "Olek";
-        bear2.wiek_ = 2;
-
-        Bear bear3 = new Bear();
-        bear3.imię = "Filipek";
-        bear3.wiek = 10;
-
-        Bear bear4 = new Bear();
-        bear4.imię = "Teddy";
-        bear4.wiek = 3;
-
+    String imie;
+    int wiek;
     }
 }
--- src/main/java/com/sda/arrpl2/Main.java
+++ src/main/java/com/sda/arrpl2/Main.java
@@ -2,6 +2,27 @@ package com.sda.arrpl2;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Zadanie 1 wykonane");
+
+
+        Bear bear1 = new Bear();
+        bear1.imie = "Marian";
+        bear1.wiek = 32;
+
+        Bear bear2 = new Bear();
+        bear2.imie = "Olek";
+        bear2.wiek = 10;
+
+        Bear bear3 = new Bear();
+        bear3.imie = "Filipek";
+        bear3.wiek = 10;
+
+        Bear bear4 = new Bear();
+        bear4.imie = "Teddy";
+        bear4.wiek = 3;
+        //sprawdzenie czy działa
+        System.out.println(bear1.imie + bear1.wiek + "oraz " + bear4.imie);
+
 
     }
 }
