6aee28bbc8167a3b43fb9076f5e16cf965927909
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-06 19:57:32 +0200

    dodana klasa osoba

--- src/main/java/com/sda/arrpl2/Main.java
+++ src/main/java/com/sda/arrpl2/Main.java
@@ -2,6 +2,5 @@ package com.sda.arrpl2;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/com/sda/arrpl2/Osoba.java
@@ -0,0 +1,12 @@
+package com.sda.arrpl2;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Osoba {
+    String imie;
+    String nazwisko;
+
+    public class Osoba {String imie,String nazwisko}
+
+}
